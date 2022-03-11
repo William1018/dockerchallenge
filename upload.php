a312b22620e257d7f30ae6bcd08e19f1599fb602
William1018 <44850658+William1018@users.noreply.github.com>
2022-03-11 05:17:44 +0000

    Update upload.php

--- upload.php
+++ upload.php
@@ -7,14 +7,7 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
     $uploadOk = 0;
-  }
 }
 
 // Check if file already exists
@@ -24,8 +17,8 @@ if (file_exists($target_file)) {
 }
 
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+if($imageFileType != "pdf" && $imageFileType != "txt") {
+  echo "Sorry, only PDF and TXT files are allowed.";
   $uploadOk = 0;
 }
 
